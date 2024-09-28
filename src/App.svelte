3f68d2f0155a29e5c731ab27bdfb6c4336d74f97
ManicMarrc <manicmarrc@gmail.com>
2024-09-28 17:08:10 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Crossword from "svelte-crossword";
+
+  const data = [
+    {
+      clue: "Nama orang yang gila",
+      answer: "AKBAR",
+      direction: "down",
+      x: 0,
+      y: 0,
+    },
+    {
+      clue: "Nama orang yang pintar",
+      answer: "ANDREA",
+      direction: "across",
+      x: 0,
+      y: 3,
+    },
+  ];
+</script>
+
+<main>
+  <Crossword {data} actions="[]">
+    <div slot="complete">
+      <h3>Kamu menang!</h3>
+    </div>
+  </Crossword>
+</main>
+
+<style></style>
